dffc7ef48f41f065268b7988fe795e702b69a88b
Raj Chaurasiya <102717203+TheRaj200@users.noreply.github.com>
2026-08-17 17:05:11 +0530

    Second Largest Number In The Array

---
+++ 20-secondLargestNumberInTheArray.js
@@ -0,0 +1,14 @@
+let arr = [23, 34, 53,12 ,55 ,80,76];
+let max = Math.max(arr[0], arr[1]);
+let smax = Math.min(arr[0], arr[1]);
+
+for(let i = 2 ; i<arr.length ; i++){
+    
+    if(arr[i]> max){
+        smax = max;
+        max = arr[i];
+    }else if(arr[i]>smax){
+        smax= arr[i];
+    }
+}
+console.log(smax)
