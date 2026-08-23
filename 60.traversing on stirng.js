ddcc42b0face707c96c4deec8be0d4c59449cd00
Raj Chaurasiya <102717203+TheRaj200@users.noreply.github.com>
2026-08-24 00:17:52 +0530

    reverse the string

---
+++ 60.traversing on stirng.js
@@ -0,0 +1,5 @@
+let val = "hello"
+
+for(let i=0 ; i<val.length; i++){
+    console.log(val[i])
+}
---
+++ 61. reverse the string.js
@@ -0,0 +1,7 @@
+let val = "hello"
+let rev = "";
+
+for(let i = val.length-1 ; i>=0; i--){
+    rev= rev + val[i]
+}
+console.log(rev)
